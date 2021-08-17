a98f738ba30ca0bf96ffc6ea9de770d4c93c9907
Mohammad Nawazish Khan <md.nawazish.khan@gmail.com>
2021-08-18 01:47:20 +0530

    First commit

---
+++ src/components/InputArea.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function InputArea() {
+  return <input class="search-box" type="text" name="search-box"></input>
+}
+
+export default InputArea;
---
+++ src/components/SearchLabel.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+function SearchLabel() {
+  return (
+      <div class="search-label">
+        <h2>Search:</h2>
+      </div>
+    );
+}
+export default SearchLabel;
